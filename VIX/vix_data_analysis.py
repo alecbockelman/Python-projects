0addff77bdf2d58fd3098676911bb65dc5af87dd
alecbockelman <92006734+alecbockelman@users.noreply.github.com>
2021-10-08 01:13:39 -0500

    Update vix_data_analysis.py

--- VIX/vix_data_analysis.py
+++ VIX/vix_data_analysis.py
@@ -19,19 +19,19 @@ def hist_average_close (data):
 def hist_average_std_dev (data):
     hist_std_dev =[]
     for row in data:
-        hist_std_dev = (data['CLOSE'].rolling(30).std())
-        for i in range(30):
+        hist_std_dev = (data['CLOSE'].rolling(20).std())
+        for i in range(20):
             del hist_std_dev[i]
         avg_hist_std_dev = hist_std_dev.mean()
     return avg_hist_std_dev
  
 def vix_current():
     VIX = yf.Ticker('^VIX')
-    df_curr = data.iloc[len(data)-30:,:]
+    df_curr = data.iloc[len(data)-20:,:]
     curr_std_dev = df_curr['CLOSE'].std()
     return VIX.info['regularMarketPrice'], curr_std_dev
 
-df_curr = data.iloc[len(data)-30:,:]
+df_curr = data.iloc[len(data)-20:,:]
 df_curr_datetime = df_curr['DATE']
 
 
@@ -42,13 +42,13 @@ fig, ax = plt.subplots(figsize=(12, 12))
 plt.plot(x,y)
 plt.ylabel('VIX Price')
 plt.xlabel('DATE')
-plt.title('VIX Price Last 30 Market Days')
+plt.title('VIX Price Last 20 Market Days')
 
 
 plt.show()
 
 print('The historical average vix close is:     ', hist_average_close(data))
-print('The average historical 30 day vix std dev is:    ', hist_average_std_dev(data))
-print('Current VIX price: ',  vix_current()[0], '       Past 30 day avg std dev: ', vix_current()[1])
+print('The average historical 20 day vix std dev is:    ', hist_average_std_dev(data))
+print('Current VIX price: ',  vix_current()[0], '       Past 20 day avg std dev: ', vix_current()[1])
 
 
